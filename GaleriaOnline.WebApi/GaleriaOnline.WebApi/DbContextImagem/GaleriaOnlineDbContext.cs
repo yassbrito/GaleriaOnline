c74dd9bb798857f8f277ca4b7759acf567802326
Yasmin Brito <yasmin.b.costa7@aluno.senai.br>
2025-08-19 11:04:26 -0300

    :tada: primeiro commit projeto

---
+++ GaleriaOnline.WebApi/GaleriaOnline.WebApi/DbContextImagem/GaleriaOnlineDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using GaleriaOnline.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaleriaOnline.WebApi.DbContextImagem;
+
+public partial class GaleriaOnlineDbContext : DbContext
+{
+    public GaleriaOnlineDbContext()
+    {
+    }
+
+    public GaleriaOnlineDbContext(DbContextOptions<GaleriaOnlineDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Imagem> Imagens { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=NOTE31-S28\\SQLEXPRESS;Database=GaleriaOnlineDB;user ID= sa; Pwd= Senai@134; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Imagem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Imagens__3214EC0711103966");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ GaleriaOnline.WebApi/GaleriaOnline.WebApi/Models/Imagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaleriaOnline.WebApi.Models;
+
+public partial class Imagem
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(255)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(500)]
+    public string Caminho { get; set; } = null!;
+}
